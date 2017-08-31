56a1a6a34f425b4528cf4f325132306478104ba8
FyS <ballandFyS@gmail.com>
2017-09-01 00:28:14 +0200

    [Gateway][MQ] : up

--- test/TestUnitMQ.cpp
+++ test/TestUnitMQ.cpp
@@ -3,11 +3,11 @@
 //
 
 #define BOOST_TEST_MODULE test_bus
+#define BOOST_TEST_DYN_LINK
 
 #include <boost/test/unit_test.hpp>
-#include <bus/FysBus.hh>
 #include <boost/thread.hpp>
-#include <thread>
+#include "FysBusTest.hh"
 
 /**
  * ------------------------------------------------------
@@ -17,19 +17,22 @@
 
 BOOST_AUTO_TEST_CASE( test_bus_ini )
 {
-    fys::mq::FysBus<std::string, 100> bus("/home/FyS/ClionProjects/FySMQ/docs/test_bus_inifile.ini");
+    fys::mq::test::FysBusTest<std::string, 100> bus("/home/FyS/ClionProjects/FreeYourSoul_Server/FySMQ/resource/test_bus_inifile.ini");
+    bus.validateBusConfiguration(2);
 }
 
-BOOST_AUTO_TEST_CASE_EXPECTED_FAILURES( test_bus_ini_error1, 1 )
+BOOST_AUTO_TEST_CASE_EXPECTED_FAILURES( test_bus_ini_error1, 2 )
 BOOST_AUTO_TEST_CASE( test_bus_ini_error1 )
 {
-    fys::mq::FysBus<std::string, 100> bus("/home/FyS/ClionProjects/FreeYourSoul_MQ/docs/test_bus_inifile_error1.ini");
+    fys::mq::test::FysBusTest<std::string, 100> bus("/home/FyS/ClionProjects/FreeYourSoul_Server/FySMQ/resource/test_bus_inifile_error1.ini");
+    bus.validateBusConfiguration(2);
 }
 
-BOOST_AUTO_TEST_CASE_EXPECTED_FAILURES( test_bus_ini_error2, 1 )
+BOOST_AUTO_TEST_CASE_EXPECTED_FAILURES( test_bus_ini_error2, 2 )
 BOOST_AUTO_TEST_CASE( test_bus_ini_error2 )
 {
-    fys::mq::FysBus<std::string, 100> bus("/home/FyS/ClionProjects/FySMQ/docs/test_bus_inifile_error2.ini");
+    fys::mq::test::FysBusTest<std::string, 100> bus("/home/FyS/ClionProjects/FreeYourSoul_Server/FySMQ/resource/test_bus_inifile_error2e.ini");
+    bus.validateBusConfiguration(3);
 }
 
 /**
@@ -48,14 +51,13 @@ void addinlockfreequeue(const fys::mq::QueueContainer<std::string> &container) {
 }
 
 void readLockFreeQueue() {
-    for (int i = 0; i < 10000; ++i) {
+    for (int i = 0; i < 5000; ++i) {
         fys::mq::QueueContainer<std::string> *container;
 
         container = lockFreeQueue->pop();
         usleep(100);
         if (container != NULL) {
             ++readValues;
-//            std::cout << readValues << " | ";
         }
     }
 }
@@ -73,23 +75,26 @@ void cleanTestExecution() {
     }
 }
 
-BOOST_AUTO_TEST_CASE( test_bus_exec ) {
-    initTestExecution();
-    std::cout << "begin test exec" << std::endl;
+BOOST_AUTO_TEST_CASE( test_queue_exec ) {
     fys::mq::QueueContainer<std::string> c1("A");
     fys::mq::QueueContainer<std::string> c2("B");
     fys::mq::QueueContainer<std::string> c3("C");
-    std::thread workerRead(readLockFreeQueue);
-    std::thread w1(addinlockfreequeue, c1);
-    std::thread w2(addinlockfreequeue, c2);
-    std::thread w3(addinlockfreequeue, c3);
 
-    sleep(2);
+    std::cout << "begin test exec" << std::endl;
+    c1.setOpCodeMsg(42);
+    c2.setOpCodeMsg(43);
+    c3.setOpCodeMsg(44);
+    initTestExecution();
+    boost::thread workerRead(readLockFreeQueue);
+    boost::thread w1(boost::bind(addinlockfreequeue, c1));
+    boost::thread w2(boost::bind(addinlockfreequeue, c2));
+    boost::thread w3(boost::bind(addinlockfreequeue, c3));
+
     w1.join();
     w2.join();
     w3.join();
     workerRead.join();
     cleanTestExecution();
     std::cout << "end test exec : readValue = " << readValues << std::endl;
-    BOOST_ASSERT(readValues == 4500);
+    BOOST_CHECK(readValues == 4500);
 }
